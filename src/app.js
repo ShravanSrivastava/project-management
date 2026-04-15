a2884b45d49422fd5556583329bfb68d12f5c52a
Shravan Srivastava <shravansrivastava225@gmail.com>
2026-04-16 01:10:22 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from "express"
+import cors from "cors"
+
+
+const app = express();
+
+// basic configuration
+app.use(express.json({limit : "16kb"}))
+app.use(express.urlencoded({extended:true, limit :"16kb"}))
+app.use(express.static("public"))
+
+// cors configuration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:8000",
+    credentials: true,
+    methods: ["GET","PUT", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }),
+);
+
+// import the routes
+import healthCheckRouter from "./routes/healtcheck.routes.js"
+app.use("/api/v1/healthcheck", healthCheckRouter); 
+
+
+app.get("/",(req,res) => {
+    res.send("welcome to basecampy")
+})
+
+app.get("/Instagram",(req,res) => {
+    res.send("This is an Instagram Page")
+})
+
+export default app
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path:"./.env",
+});
+
+
+
+const port = process.env.port || 3000;
+
+
+
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app Listening at http://localhost${port}`);
+    })
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error", err)
+    process.exit(1)
+  })
+
+
+let myusername = process.env.database;
+console.log("value:",myusername)
+console.log("Start of the awesome project");
