debeac6151018798193213a468bb343e91694edc
Shravan Srivastava <shravansrivastava225@gmail.com>
2026-05-03 20:11:29 +0530

    added accessToken and refreshToken in jwt

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,67 +1,91 @@
-import mongoose, {Schema} from "mongoose";
-import bcrypt from "bcrypt"
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwentoken";
 
 const userSchema = new Schema(
-    {
-        avatar: {
-            type:{
-                url: String,
-                localPath: String,
-            },
-            default: {
-                url: ``,
-                localPath: "",
-            }
-        },
-        userName: {
-            type: String,
-            required:true,
-            lowercase: true,
-            trim:true,
-            index:true,
-            unique:true
-        },
-        email:{
-            type:String,
-            required:true,
-            unique:true,
-            lowercase:true,
-            trim:true
-        },
-        fullName:{
-            type:String,
-            trim:true
-        },
-        password:{
-            type:String,
-            required:[true,"Password is required"]
-        },
-        isEmailVerified:{
-            type:Boolean,
-            default:false
-        },
-        refreshToken:{
-            type:String
-        },
-        forgetPasswordToken:{
-            type:String
-        },
-        forgotPasswordExpiry:{
-            type:Date
-        }
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: ``,
+        localPath: "",
+      },
     },
+    userName: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgetPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timeStamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
     {
-        timeStamps:true,
-    }
-)
+      _id: this._id,
+      userName: this.userName,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRES },
+  );
+};
 
-userSchema.pre("save", async function(next){
-    if(!this.isModified("password")) return next()
-    this.password = await bcrypt.hash(this.password,10)
-    next();
-})
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRES },
+  );
+};
 
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password,this.password)
-}
-export const user = mongoose.model("user",userSchema)
+export const user = mongoose.model("user", userSchema);
